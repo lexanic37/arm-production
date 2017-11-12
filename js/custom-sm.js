5c6e9079b573f964461d887ef5d609093c48a254
lexanic37 <lexanic37@gmail.com>
2017-11-12 16:13:48 +0300

    a

--- js/custom-sm.js
+++ js/custom-sm.js
@@ -1,8 +1,23 @@
-$(document).ready(function(){
-    var controller = new ScrollMagic.Controller();
-    var scene = new ScrollMagic.Scene({
-        triggerElement: '#advant0'
-    })
-    .setClassToggle('.zona','fade-in')
+var controller = new ScrollMagic.Controller();
+var Scene = new ScrollMagic.Scene({
+    triggerElement: '#advant0',
+    triggerHook: 0.1,
+})
+    .setClassToggle('#advant0','fade-in')
     .addTo(controller)
-});
+var Scene1 = new ScrollMagic.Scene({
+    triggerElement: '#advant1',
+    triggerHook: 0.1,
+})
+    .setClassToggle('#advant1','fade-in')
+    .addTo(controller)
+var Scene2 = new ScrollMagic.Scene({
+    triggerElement: '#advant2',
+    triggerHook: 0.1,
+})
+    .setClassToggle('#advant2','fade-in')
+    .addTo(controller)
+
+$('.item1')
+
+
